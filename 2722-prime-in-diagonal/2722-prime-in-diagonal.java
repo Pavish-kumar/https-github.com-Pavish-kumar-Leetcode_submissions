a7a211e57249c9f17b9a04985bff5effd4f119ce
Pavish Kumar R <pavishkumar2004@gmail.com>
2024-11-04 17:30:22 +0530

    Time: 2 ms (95.42%), Space: 59.3 MB (42.6%) - LeetHub

---
+++ 2722-prime-in-diagonal/2722-prime-in-diagonal.java
@@ -0,0 +1,42 @@
+class Solution {
+    public int diagonalPrime(int[][] nums) {
+        int n=nums.length-1;
+        int max=Integer.MIN_VALUE;
+        for(int i=0;i<=n;i++)
+        {
+            if(isPrime(nums[i][i]))
+            {
+                max=Math.max(max,nums[i][i]);
+            }
+            if(isPrime(nums[i][n-i]))
+            {
+                max=Math.max(max,nums[i][n-i]);
+            }
+        }
+        return max == Integer.MIN_VALUE ? 0 : max;
+    }
+    public boolean isPrime(int num)
+    {
+        
+        if(num<=1)
+        {
+            return false;
+        }
+        if(num<=3)
+        {
+            return true;
+        }
+        if(num%2==0||num%3==0)
+        {
+            return false;
+        }
+        for(int i=5;i*i<=num;i+=6)
+        {
+            if(num%i==0||num%(i+2)==0)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+}
