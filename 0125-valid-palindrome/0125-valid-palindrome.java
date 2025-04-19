586f062d890c284e4f536b959520ddb312a5df55
Pavish Kumar R <pavishkumar2004@gmail.com>
2025-04-19 09:34:44 +0530

    Time: 2 ms (99.04%), Space: 43 MB (83.58%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,24 +1,21 @@
 class Solution {
-    public boolean ispalin(String s)
-    {
-        for(int i=0;i<s.length()/2;i++)
-        {
-            if(s.charAt(i)!=s.charAt(s.length()-i-1))
-            {
+    public boolean isPalindrome(String s) {
+        int left=0,right=s.length()-1;
+        while(left<right){
+            if(!Character.isLetterOrDigit(s.charAt(left))){
+                left++;
+                continue;
+            }
+            if(!Character.isLetterOrDigit(s.charAt(right))){
+                right--;
+                continue;
+            }
+            if(Character.toLowerCase(s.charAt(left))!=Character.toLowerCase(s.charAt(right))){
                 return false;
             }
+            left+=1;
+            right-=1;
         }
         return true;
-    }
-    public boolean isPalindrome(String s) {
-        StringBuilder ss=new StringBuilder();
-        for(int i=0;i<s.length();i++)
-        {
-            if(Character.isLetterOrDigit(s.charAt(i)))
-            {
-                ss.append(Character.toLowerCase(s.charAt(i)));
-            }
-        }
-        return ispalin(ss.toString());
     }
 }
