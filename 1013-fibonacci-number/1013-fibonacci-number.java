3244220c6f402a39f8a18070a1e516af9de6539d
Pavish Kumar R <pavishkumar2004@gmail.com>
2025-06-18 14:26:58 +0530

    Time: 0 ms (100%), Space: 40.5 MB (39.39%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.java
+++ 1013-fibonacci-number/1013-fibonacci-number.java
@@ -1,7 +1,12 @@
 class Solution {
-    public int fib(int n) {
-        double gr=(1+Math.pow(5,0.5))/2;
-        double res=Math.pow(gr,n)/Math.pow(5,0.5);
-        return (int)Math.round(res);
+  public int fib(int n) {
+    if (n <= 1) {
+        return n;
     }
+    double sqrt5 = Math.sqrt(5);
+    double phi = (1 + sqrt5) / 2;
+    double psi = (1 - sqrt5) / 2;
+    double result = (Math.pow(phi, n) - Math.pow(psi, n)) / sqrt5;
+    return (int)Math.round(result);
+  }
 }
