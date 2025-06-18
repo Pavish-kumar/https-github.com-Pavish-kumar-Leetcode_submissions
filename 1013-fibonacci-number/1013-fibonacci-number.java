9347eed23eff21de6d1c6393d4dafb033ca60816
Pavish Kumar R <pavishkumar2004@gmail.com>
2025-06-18 14:08:55 +0530

    Time: 0 ms (100%), Space: 40.2 MB (80.37%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.java
+++ 1013-fibonacci-number/1013-fibonacci-number.java
@@ -1,14 +1,7 @@
 class Solution {
     public int fib(int n) {
-        if(n<=1){
-            return n;
-        }
-       int[] dp=new int[n+1];
-       dp[0]=0;
-       dp[1]=1;
-       for(int i=2;i<=n;i++){
-        dp[i]=dp[i-1]+dp[i-2];
-       }
-       return dp[n];
+        double gr=(1+Math.pow(5,0.5))/2;
+        double res=Math.pow(gr,n)/Math.pow(5,0.5);
+        return (int)Math.round(res);
     }
 }
