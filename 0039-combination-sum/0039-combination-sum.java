defdba3bc858b9b3098f8b59e4324d7093955a7c
Pavish Kumar R <pavishkumar2004@gmail.com>
2025-06-25 14:44:51 +0530

    Time: 1 ms (99.89%), Space: 44.9 MB (21.2%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.java
+++ 0039-combination-sum/0039-combination-sum.java
@@ -6,17 +6,16 @@ class Solution {
         return res;
     }
     private void helper(int ind,List<Integer> list,int[] nums,int target,List<List<Integer>> res){
-        if(ind==nums.length){
-            if(target==0){
-                res.add(new ArrayList<>(list));
-            }
+        if(target==0){
+            res.add(new ArrayList<>(list));
             return;
         }
-        if(target>=nums[ind]){
-            list.add(nums[ind]);
-            helper(ind,list,nums,target-nums[ind],res);
-            list.remove(list.size()-1);
+        for(int i=ind;i<nums.length;i++){
+            if(target>=nums[i]){
+                list.add(nums[i]);
+                helper(i,list,nums,target-nums[i],res);
+                list.remove(list.size()-1);
+            }
         }
-        helper(ind+1,list,nums,target,res);
     }
 }
