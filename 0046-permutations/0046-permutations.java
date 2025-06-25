d6b3a45102439654f52718dc07c73f61cd998382
Pavish Kumar R <pavishkumar2004@gmail.com>
2025-06-25 13:56:51 +0530

    Time: 1 ms (93.33%), Space: 44.6 MB (45.75%) - LeetHub

--- 0046-permutations/0046-permutations.java
+++ 0046-permutations/0046-permutations.java
@@ -3,18 +3,21 @@ class Solution {
         List<List<Integer>> res=new ArrayList<>();
         List<Integer> list=new ArrayList<>();
         boolean[] visited=new boolean[nums.length];
-        helper(nums,list,res,visited);
+        helper(nums,res,list,visited);
         return res;
     }
-    public void helper(int[] nums,List<Integer> list,List<List<Integer>> res, boolean[] visited){
+    private void helper(int[] nums,List<List<Integer>> res,List<Integer> list,boolean[] visited){
         if(list.size()==nums.length){
             res.add(new ArrayList<>(list));
+            return;
         }
         for(int i=0;i<nums.length;i++){
-            if(visited[i]) continue;
+            if(visited[i]){
+                continue;
+            }
             list.add(nums[i]);
             visited[i]=true;
-            helper(nums,list,res,visited);
+            helper(nums,res,list,visited);
             list.remove(list.size()-1);
             visited[i]=false;
         }
