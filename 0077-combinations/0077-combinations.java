169db1e75d630d025ce387d498cde09baf119fd5
Pavish Kumar R <pavishkumar2004@gmail.com>
2025-06-25 14:09:46 +0530

    Time: 26 ms (16.76%), Space: 94.7 MB (42.78%) - LeetHub

--- 0077-combinations/0077-combinations.java
+++ 0077-combinations/0077-combinations.java
@@ -1,18 +1,17 @@
 class Solution {
     public List<List<Integer>> combine(int n, int k) {
-        List<List<Integer>> res=new ArrayList<>();
+        List<List<Integer>>res=new ArrayList<>();
         List<Integer> list=new ArrayList<>();
-        helper(1,n,list,res,k);
+        helper(1,n,k,list,res);
         return res;
     }
-    public void helper(int st,int n,List<Integer> list,List<List<Integer>> res,int k){
+    public void helper(int st,int en,int k,List<Integer> list,List<List<Integer>> res){
         if(list.size()==k){
             res.add(new ArrayList<>(list));
-            return;
         }
-        for(int i=st;i<=n;i++){
+        for(int i=st;i<=en;i++){
             list.add(i);
-            helper(i+1,n,list,res,k);
+            helper(i+1,en,k,list,res);
             list.remove(list.size()-1);
         }
     }
